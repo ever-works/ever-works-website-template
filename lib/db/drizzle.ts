0ae54ac7075030f497f2e43f71b88647a4e33a78
Paradoxe Ngwasi <paradoxngwasi@gmail.com>
2025-04-16 13:41:13 +0000

    refactor: remove DATABASE_URL check from drizzle.ts

--- lib/db/drizzle.ts
+++ lib/db/drizzle.ts
@@ -2,10 +2,6 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL environment variable is not set");
-}
-
 const globalForDb = globalThis as unknown as {
   conn: postgres.Sql | undefined;
 };
