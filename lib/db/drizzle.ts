d35d22dc7fef935feb816670defd7ef07bf0ec30
Paradoxe Ngwasi <paradoxngwasi@gmail.com>
2025-04-16 13:45:54 +0000

    refactor: comment out DATABASE_URL check in drizzle.ts

--- lib/db/drizzle.ts
+++ lib/db/drizzle.ts
@@ -2,6 +2,11 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
 
+// For now, let's comment this out to avoir a build error in the automated environment (e.g a GH actions)
+// if (!process.env.DATABASE_URL) {
+//   throw new Error("DATABASE_URL environment variable is not set");
+// }
+
 const globalForDb = globalThis as unknown as {
   conn: postgres.Sql | undefined;
 };
