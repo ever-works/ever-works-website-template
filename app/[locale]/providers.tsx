fd4724cad761b7adb9e2ad9dd2a299852d2432ce
Innocent-Akim <gentilakili98@gmail.com>
2025-05-15 22:18:28 +0200

    fix: sugg codeRabbit

--- app/[locale]/providers.tsx
+++ app/[locale]/providers.tsx
@@ -1,23 +1,32 @@
 "use client";
 
-import type { Config } from '@/lib/content';
-import { HeroUIProvider } from '@heroui/react';
-import { ConfigProvider } from './config';
-import { ThemeProvider } from 'next-themes';
-import ErrorProvider from '@/components/error-provider';
+import type { Config } from "@/lib/content";
+import { HeroUIProvider } from "@heroui/react";
+import { ConfigProvider } from "./config";
+import { ThemeProvider } from "next-themes";
+import ErrorProvider from "@/components/error-provider";
+import { LayoutThemeProvider } from "@/components/context";
 
-import { LayoutThemeProvider } from "@/components/context/LayoutThemeContext";
-
-export function Providers({ config, children}: { config: Config, children: React.ReactNode }) {
+export function Providers({
+  config,
+  children,
+}: {
+  config: Config;
+  children: React.ReactNode;
+}) {
   return (
     <LayoutThemeProvider>
       <ErrorProvider>
         <ConfigProvider config={config}>
-          <ThemeProvider enableSystem={true} attribute="class" defaultTheme="dark">
+          <ThemeProvider
+            enableSystem={true}
+            attribute="class"
+            defaultTheme="dark"
+          >
             <HeroUIProvider>{children}</HeroUIProvider>
           </ThemeProvider>
         </ConfigProvider>
       </ErrorProvider>
     </LayoutThemeProvider>
-  )
+  );
 }
