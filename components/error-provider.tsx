2be35569cab7b94ec686cd509eef46bae8b1385c
Innocent-Akim <gentilakili98@gmail.com>
2025-07-12 12:50:53 +0100

    fix: correct ErrorBoundary import in error-provider
    - Change from default import to named import
    - Fixes TypeScript build error about missing default export

--- components/error-provider.tsx
+++ components/error-provider.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useEffect } from 'react';
-import ErrorBoundary from './error-boundary';
+import { ErrorBoundary } from './error-boundary';
 
 interface ErrorProviderProps {
   children: React.ReactNode;
