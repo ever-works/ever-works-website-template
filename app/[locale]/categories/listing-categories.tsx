cad5a35f86926c5048df7515dcc721b1eb23d8d6
ariefgp <arief.gusti@gmail.com>
2025-07-12 09:29:28 +0700

    Remove hardcoded false condition or clarify intent.

--- app/[locale]/categories/listing-categories.tsx
+++ app/[locale]/categories/listing-categories.tsx
@@ -53,7 +53,7 @@ export default function ListingCategories(props: ListingCategoriesProps) {
           tags={props.tags}
         />
       )}
-      {paginationType === "standard" && false && (
+      {paginationType === "standard" && (
         <footer className="flex items-center justify-center">
           <Paginate
             basePath={props.basePath}
