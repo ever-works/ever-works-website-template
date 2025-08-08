8105241099c185659afb328efba05b21b7642d56
ariefgp <arief.gusti@gmail.com>
2025-08-08 21:04:25 +0700

    cleanup: remove old client migration files
    - Delete 0010_add_client_fields.sql (redundant - client fields now in client_profiles table)
    - Delete 0007_fix_clients_username_constraint.sql (references non-existent clients table)
    - Clean up migration files that could cause conflicts with new schema

--- lib/db/migrations/0007_fix_clients_username_constraint.sql
+++
@@ -1,5 +0,0 @@
--- Add NOT NULL constraint to username field in clients table
-ALTER TABLE "clients" ALTER COLUMN "username" SET NOT NULL;
-
--- Add NOT NULL constraint to username field in accounts table
-ALTER TABLE "accounts" ALTER COLUMN "username" SET NOT NULL; 
