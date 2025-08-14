dd8f30110375a961a42bfb0df943bd9816b7c3f3
ariefgp <arief.gusti@gmail.com>
2025-08-14 21:55:42 +0700

    remove unused dashboard

--- app/[locale]/dashboard/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function DashboardPage() {
-  // Redirect old dashboard route to new client dashboard
-  redirect('/client/dashboard');
-}
