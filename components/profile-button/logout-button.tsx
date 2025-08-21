c2c92fc1c985791961be2eafb92ae1a8e98d2f05
ariefgp <arief.gusti@gmail.com>
2025-08-21 16:42:58 +0700

    a11y(logout-button): hide decorative LogOut and Zap icons from assistive tech

--- components/profile-button/logout-button.tsx
+++ components/profile-button/logout-button.tsx
@@ -17,13 +17,13 @@ function LogoutButton({ onLogout, logoutText }: LogoutButtonProps) {
       role="menuitem"
     >
       <div className="flex items-center justify-center w-10 h-10 mr-3 rounded-xl bg-gradient-to-br from-red-100 to-pink-100 dark:from-red-900/30 dark:to-pink-900/30 group-hover:from-red-200 group-hover:to-pink-200 dark:group-hover:from-red-900/40 dark:group-hover:to-pink-900/40 transition-all duration-200">
-        <LogOut className="h-5 w-5 text-red-600 dark:text-red-400" />
+        <LogOut aria-hidden="true" className="h-5 w-5 text-red-600 dark:text-red-400" />
       </div>
       <div className="flex-1">
         <span className="font-semibold">{logoutText}</span>
         <p className="text-xs text-gray-500 dark:text-gray-400">Sign out of your account</p>
       </div>
-      <Zap className="h-4 w-4 text-gray-400 opacity-0 group-hover:opacity-100 transition-opacity duration-200" />
+      <Zap aria-hidden="true" className="h-4 w-4 text-gray-400 opacity-0 group-hover:opacity-100 transition-opacity duration-200" />
     </button>
   );
 }
