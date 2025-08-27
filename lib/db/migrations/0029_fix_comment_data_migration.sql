1654a987ff98e192350f05ae6ba02ee195e1df62
ariefgp <arief.gusti@gmail.com>
2025-08-27 10:57:05 +0700

    fix: add data migration for comment foreign key relationships

---
+++ lib/db/migrations/0029_fix_comment_data_migration.sql
@@ -0,0 +1,25 @@
+-- Migration 0029: Fix comment data migration to reference client_profiles.id
+-- This ensures comments.userId references the correct client_profiles.id values
+
+-- Update comments.userId to reference client_profiles.id instead of users.id
+-- This fixes the data migration issue where comments still reference users.id
+-- but foreign key constraints now point to client_profiles.id
+UPDATE "comments" 
+SET "userId" = cp."id"
+FROM "client_profiles" cp
+WHERE "comments"."userId" = cp."userId"
+  AND cp."userId" IS NOT NULL;
+
+-- Update votes.user_id to reference client_profiles.id instead of users.id
+UPDATE "votes" 
+SET "user_id" = cp."id"
+FROM "client_profiles" cp
+WHERE "votes"."user_id" = cp."userId"
+  AND cp."userId" IS NOT NULL;
+
+-- Update activityLogs.userId to reference client_profiles.id instead of users.id
+UPDATE "activityLogs" 
+SET "userId" = cp."id"
+FROM "client_profiles" cp
+WHERE "activityLogs"."userId" = cp."userId"
+  AND cp."userId" IS NOT NULL;
