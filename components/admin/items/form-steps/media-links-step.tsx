c06faba8ded1cb31d2cf27f21e024c22737bdc44
ariefgp <arief.gusti@gmail.com>
2025-09-30 21:41:08 +0700

    style: add dark mode support to icon preview container in media-links step

--- components/admin/items/form-steps/media-links-step.tsx
+++ components/admin/items/form-steps/media-links-step.tsx
@@ -158,7 +158,7 @@ export function MediaLinksStep({
             </div>
 
             {/* Icon Preview */}
-            <div className="w-16 h-16 border-2 border-dashed border-gray-300 rounded-lg flex items-center justify-center bg-gray-50">
+            <div className="w-16 h-16 border-2 border-dashed border-gray-300 dark:border-gray-600 rounded-lg flex items-center justify-center bg-gray-50 dark:bg-gray-800">
               {previewImage ? (
                 <img
                   src={previewImage}
