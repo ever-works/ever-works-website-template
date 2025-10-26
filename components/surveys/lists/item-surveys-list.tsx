24e974043c0f7e6a74bc919638d8f56d0a7496cd
Chetan Khandla <ckhandla94@gmail.com>
2025-10-26 09:59:38 +0530

    Update item-surveys-list.tsx

--- components/surveys/lists/item-surveys-list.tsx
+++ components/surveys/lists/item-surveys-list.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { surveyApiClient } from '@/lib/api/survey-api.client';
 import type { Survey } from '@/lib/db/schema';
