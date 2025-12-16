49c87ec5716bf468ca77befca4816bc2ce66a276
joel-kalema <joelkalema63@gmail.com>
2025-12-17 00:06:42 +0200

    refactor(collection-detail): remove the nav to align with other design pages

--- components/collections/collection-detail.tsx
+++ components/collections/collection-detail.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useMemo, useState } from "react";
 import { useTranslations } from "next-intl";
-import Link from "next/link";
 import { Collection } from "@/types/collection";
 import { ItemData, Tag } from "@/lib/content";
 import { Container, useContainerWidth } from "@/components/ui/container";
@@ -155,70 +154,7 @@ function CollectionDetailContent(props: CollectionDetailProps) {
         }
         className="min-h-screen text-center"
       >
-        <Container maxWidth="7xl" padding="default" useGlobalWidth>
-          {/* Breadcrumb */}
-          <nav className="flex mb-8 justify-center" aria-label="Breadcrumb">
-            <ol className="inline-flex items-center space-x-1 md:space-x-3">
-              <li className="inline-flex items-center text-black dark:text-white">
-                <Link
-                  href="/"
-                  className="inline-flex items-center text-sm font-medium hover:text-theme-primary transition-colors duration-300"
-                >
-                  <svg
-                    className="w-4 h-4 mr-2"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
-                  </svg>
-                  {t("common.HOME")}
-                </Link>
-              </li>
-              <li>
-                <div className="flex items-center text-black dark:text-white">
-                  <svg
-                    className="w-6 h-6 text-gray-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
-                  <Link
-                    href="/collections"
-                    className="ml-1 text-sm font-medium md:ml-2 hover:text-theme-primary transition-colors duration-300"
-                  >
-                    {t("common.COLLECTION")}
-                  </Link>
-                </div>
-              </li>
-              <li>
-                <div className="flex items-center text-black dark:text-white">
-                  <svg
-                    className="w-6 h-6 text-gray-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
-                  <span className="ml-1 text-sm font-medium md:ml-2">
-                    {collection.name}
-                  </span>
-                </div>
-              </li>
-            </ol>
-          </nav>
-
+        <Container maxWidth="7xl" padding="default" useGlobalWidth className="py-0 mt-0">
           <div className={LAYOUT_STYLES.mainContainer}>
             <div className="w-full">
               {/* Tags Section - Mobile version */}
