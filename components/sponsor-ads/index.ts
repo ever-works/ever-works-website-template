cc56dfdc172422bd944b85c2724cd49a07114729
ariefgp <arief.gusti@gmail.com>
2025-12-17 08:53:44 +0700

    feat(sponsor-ads): add new display components to exports

--- components/sponsor-ads/index.ts
+++ components/sponsor-ads/index.ts
@@ -1,2 +1,5 @@
 // Sponsor Ads Components
 export { SponsorForm } from "./sponsor-form";
+export { SponsorBadge } from "./sponsor-badge";
+export { SponsorCard } from "./sponsor-card";
+export { SidebarSponsor } from "./sidebar-sponsor";
